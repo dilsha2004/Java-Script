31758d34074befd14d6f9d492536ca406b6e1b2c
dilsha2004 <dilshajelish2004@gmail.com>
2026-06-24 14:22:28 +0530

    reverse an array

--- reversearray.js
+++ reversearray.js
@@ -1,12 +1,14 @@
-const reverseArray = arr => [...arr].reverse();
-const reverseArrayInPlace = arr => {
+const reverseArray = (arr) => [...arr].reverse()
+const reverseArrayInPlace = (arr) => {
   for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+    const tmp = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = tmp
   }
-};
-let myArray = ["A", "B", "C"];
-console.log(reverseArray(myArray)); 
-console.log(myArray);               
-let arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue);           
+}
+let myArray = ["A", "B", "C"]
+console.log(reverseArray(myArray))
+console.log(myArray)
+let arrayValue = [1, 2, 3, 4, 5]
+reverseArrayInPlace(arrayValue)
+console.log(arrayValue)
